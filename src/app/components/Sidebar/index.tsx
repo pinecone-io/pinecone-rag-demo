7da2a76d510ef8b835cdb2a7a2e2a6d856cab9e2
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2024-01-11 19:55:53 -0800

    fixed vercel build css issue

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -161,16 +161,17 @@ export const Sidebar: React.FC = () => {
                 }
               }
             }} >
-            <MenuItem value="markdown" className="max-w-full overflow-auto whitespace-normal">
-              <div className="flex flex-col ">
-                <div className="font-bold">Markdown chunking</div>
-                <div className="text-[14px]">Markdown chunking leverages the structure of the document itself, creating chunks that correlate to the markdown semantics of the content. The crawler converts the URL content to markdown and then applies the markdown chunking methods on the content.</div>
+            {/* Using tailwind here resulted in broken css when deployed on Vercel */}
+            <MenuItem value="markdown" style={{ maxWidth: '100%', overflow: 'auto', whiteSpace: 'normal' }}>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <div style={{ fontWeight: 'bold' }}>Markdown chunking</div>
+                <div style={{ fontSize: 14 }}>Markdown chunking leverages the structure of the document itself, creating chunks that correlate to the markdown semantics of the content. The crawler converts the URL content to markdown and then applies the markdown chunking methods on the content.</div>
               </div>
             </MenuItem>
-            <MenuItem value="recursive" className="max-w-full overflow-auto whitespace-normal">
-              <div className="flex flex-col">
-                <div className="font-bold">Recursive Chunking</div>
-                <div className="text-[14px]">With recursive chunking, the text will be divided into smaller parts in each recursion step, based on the chunk size you define. The overlap will ensure that the chunks include content found in adjacent chunks so that no content is lost.  </div>
+            <MenuItem value="recursive" style={{ maxWidth: '100%', overflow: 'auto', whiteSpace: 'normal' }}>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <div style={{ fontWeight: 'bold' }}>Recursive Chunking</div>
+                <div style={{ fontSize: 14 }}>With recursive chunking, the text will be divided into smaller parts in each recursion step, based on the chunk size you define. The overlap will ensure that the chunks include content found in adjacent chunks so that no content is lost.  </div>
               </div>
 
             </MenuItem>
