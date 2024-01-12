d38509f0e2c4d8620da8ddc83818edc4912b0aa3
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2024-01-11 20:33:05 -0800

    updated URLS

--- src/app/components/Sidebar/urls.ts
+++ src/app/components/Sidebar/urls.ts
@@ -1,18 +1,24 @@
 export const urls = [{
-  url: "https://e360.yale.edu/digest/indonesia-malaysia-deforestation",
-  title: "Indonesia Deforestation",
+  url: "https://www.artificialintelligence-news.com/2024/01/02/us-chief-justice-ai-wont-replace-judges-will-transform-our-work/",
+  title: "AI won't replace judges",
   seeded: false,
   loading: false,
 },
 {
-  url: "https://cleantechnica.com/2023/06/29/solar-82-of-power-capacity-growth-in-india-in-2022/",
-  title: "Solar Power in India",
+  url: "https://www.wired.com/story/fast-forward-toyota-robots-learning-housework/",
+  title: "Toyota's Robots are Learning Housework",
   seeded: false,
   loading: false,
 },
 {
-  url: "https://www.cbssports.com/nba/news/matisse-thybulle-to-stay-with-blazers-who-match-mavericks-three-year-33-million-deal-per-report/",
-  title: "Matisee Thybulle",
+  url: "https://www.wired.com/story/synthetic-data-is-a-dangerous-teacher/",
+  title: "Synthetic Data Is a Dangerous Teacher",
+  seeded: false,
+  loading: false,
+},
+{
+  url: "https://www.wired.com/story/staying-one-step-ahead-of-hackers-when-it-comes-to-ai/",
+  title: "Staying Ahead of Hackers When It Comes to AI",
   seeded: false,
   loading: false,
 }]
